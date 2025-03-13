e2cf9fc1c222a3c231e2c78f86e231adc5c55edb
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-03-13 13:33:39 +0100

    update config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,11 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pdb-handler/",
   plugins: [react(), tailwindcss()],
   server: {
     fs: {
       // Specify `/pkg` here so we can load the types
       allow: ["..", "../../pkg"],
     },
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
